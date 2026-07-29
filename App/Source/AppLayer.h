1f2fd17c2d203f2d0819e1c15ee0f72d62e2961b
0suyog <dahalsunischit@gmail.com>
2026-07-29 15:05:23 +0545

    added a quad

--- App/Source/AppLayer.h
+++ App/Source/AppLayer.h
@@ -12,10 +12,11 @@ public:
     float vertices[] = {
         -0.5f, -0.5f, 0.0f, // A
         0.5f,  -0.5f, 0.0f, // B
-        0.0f,  0.5f,  0.0f  // C
+        0.5f,  0.5f,  0.0f, // C
+        -0.5f, 0.5f,  0.0f, // D
     };
 
-    uint indices[] = {0, 1, 2};
+    uint indices[] = {0, 1, 2, 0, 2, 3};
 
     glGenBuffers(1, &vbo);
     glGenBuffers(1, &ebo);
@@ -24,8 +25,7 @@ public:
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, sizeof(float), GL_FLOAT, GL_TRUE,
-                          3 * sizeof(float), 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
@@ -38,7 +38,7 @@ public:
     glClear(GL_COLOR_BUFFER_BIT);
     basicshader.use();
     glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
   }
 
   void OnEvent(Core::Event &e) override {
